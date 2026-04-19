6de5e3b114466b9589530c531e298c4b94deffac
Alfonso David Araujo <alfonsodaraujo@unicesa.edu.co>
2026-04-19 12:09:15 -0500

    fead:implementacion de clase Subasta

---
+++ src/main/java/CapaModelo/Subasta.java
@@ -0,0 +1,32 @@
+package CapaModelo;
+import java.time.LocalDateTime;
+
+public class Subasta {
+    private int idAuto;
+    private double precioActual;
+    private String ultimoPostor;
+    private LocalDateTime fechaInicio;
+    private LocalDateTime fechaFin;
+
+    public Subasta() {}
+
+    public Subasta(int idAuto, double precioInicial) {
+        this.idAuto = idAuto;
+        this.precioActual = precioInicial;
+        this.ultimoPostor = "Sin ofertas";
+        this.fechaInicio = LocalDateTime.now();
+        this.fechaFin = this.fechaInicio.plusHours(24); // Duraciin de 24 horas
+    }
+
+    public boolean estaActiva() {
+        return LocalDateTime.now().isBefore(fechaFin);
+    }
+
+    public int getIdAuto() { return idAuto; }
+    public void setIdAuto(int idAuto) { this.idAuto = idAuto; }
+    public double getPrecioActual() { return precioActual; }
+    public void setPrecioActual(double precioActual) { this.precioActual = precioActual; }
+    public String getUltimoPostor() { return ultimoPostor; }
+    public void setUltimoPostor(String ultimoPostor) { this.ultimoPostor = ultimoPostor; }
+    public LocalDateTime getFechaFin() { return fechaFin; }
+}
