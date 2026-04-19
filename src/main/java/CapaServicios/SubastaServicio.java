a5c8870b1e5aadef8309ec5ee688277a07f5c410
Alfonso David Araujo <alfonsodaraujo@unicesa.edu.co>
2026-04-19 12:32:02 -0500

    fead:implementacion de clase SubastaServicio

---
+++ src/main/java/CapaServicios/SubastaServicio.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CapaServicios;
+import CapaExcepciones.Excepciones;
+import java.sql.SQLException;
+
+public interface SubastaServicio {
+    void iniciarSubasta(int idAuto, double precioBase) throws SQLException;
+    void realizarPuja(int idAuto, double monto, String usuario) throws Excepciones, SQLException;
+}
