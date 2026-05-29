8929ed87c726bb009699ef109829f977bbe8b3ce
Eriri1111 <alfonsodaraujo@uncisar.edu.co>
2026-05-28 23:27:07 -0500

    Fead:implementacion de datafono virtual y pago con Wompi

---
+++ src/main/java/CapaServicios/PagoWompi.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CapaServicios;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class PagoWompi implements MetodoPago {
+   
+    private final String LINK_WOMPI = "https://checkout.wompi.co/l/test_VPOS_1IcxE5";
+
+    @Override
+    public String prepararPasarela(double monto, String descripcion) {
+       
+        return LINK_WOMPI + "?amount=" + (int)(monto * 100) + "&reference=AUTO-" + System.currentTimeMillis();
+    }
+
+    public void abrirPasarela() {
+        try {
+            Desktop.getDesktop().browse(new URI(LINK_WOMPI));
+        } catch (IOException | URISyntaxException e) {
+            System.err.println("Error al conectar con Wompi: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void procesarPago(double monto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
