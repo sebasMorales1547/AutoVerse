eb123d1761f5193393a3fed8fb215bb0bb5c4ba4
Eriri1111 <alfonsodaraujo@unicesar.edu.co>
2026-05-30 13:45:32 -0500

    fead:implementacion de la gestion de roles

--- src/main/java/CapaServicios/UsuarioServicio.java
+++ src/main/java/CapaServicios/UsuarioServicio.java
@@ -4,38 +4,50 @@
  */
 package CapaServicios;
 
-import CapaDao.UsuarioDao; 
+import CapaDao.UsuarioDao;
 import CapaExcepciones.Excepciones;
+import CapaModelo.RolUsuario;
 import CapaModelo.Usuarios;
+import CapaUtilidades.SesionActual;
 import java.sql.SQLException;
 
 public class UsuarioServicio {
     private final UsuarioDao usuarioDao = new UsuarioDao();
 
     public void registrarNuevoUsuario(String user, String pass, String email) throws Excepciones, SQLException {
-        if (user.isEmpty() || pass.isEmpty() || email.isEmpty()) {
+        if (user.isEmpty() || pass.isEmpty() || email.isEmpty())
             throw new Excepciones("Todos los campos son obligatorios.");
-        }
-        if (user.length() < 4) {
+        if (user.length() < 4)
             throw new Excepciones("El nombre de usuario debe tener al menos 4 caracteres.");
-        }
-        if (pass.length() < 8) {
+        if (pass.length() < 8)
             throw new Excepciones("La contraseña debe tener al menos 8 caracteres.");
-        }
-        
+        if (!email.contains("@"))
+            throw new Excepciones("El correo electrónico no es válido.");
+
         Usuarios nuevo = new Usuarios(user, pass, email);
         usuarioDao.registrarUsuario(nuevo);
     }
 
     public Usuarios iniciarSesion(String user, String pass) throws Excepciones, SQLException {
-        if (user.isEmpty() || pass.isEmpty()) {
+        if (user.isEmpty() || pass.isEmpty())
             throw new Excepciones("Por favor, ingrese sus credenciales.");
-        }
+
         Usuarios usuario = usuarioDao.validarLogin(user, pass);
-        
-        if (usuario == null) {
+        if (usuario == null)
             throw new Excepciones("Credenciales incorrectas. Intente de nuevo.");
-        }
-        return usuario; 
+//se hace resgitro de la nueva sesion
+        SesionActual.iniciarSesion(usuario);
+        return usuario;
+    }
+
+    public void cerrarSesion() {
+        SesionActual.cerrarSesion();
+    }
+
+    public void cambiarRolUsuario(int cedulaObjetivo, RolUsuario nuevoRol) throws Excepciones, SQLException {
+        if (!SesionActual.esAdmin())
+            throw new Excepciones("No tienes permisos para cambiar roles de usuario.");
+
+        usuarioDao.actualizarRol(cedulaObjetivo, nuevoRol);
     }
 }
