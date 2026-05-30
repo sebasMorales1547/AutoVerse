dfb5cbb4294c3ec53768015415767fdd58dbc266
Eriri1111 <alfonsodaraujo@unicesar.edu.co>
2026-05-30 14:30:30 -0500

    fead:implementacion de configuracion para eniar correos en tiempo real

---
+++ src/main/java/CapaUtilidades/EmailConfig.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CapaUtilidades;
+//configuracion para enviar correos en tiempo real...
+public class EmailConfig {
+    public static final String CORREO_REMITENTE = "tuemail@gmail.com";  // <-- cambia esto
+    public static final String CLAVE_APP        = "xxxx xxxx xxxx xxxx"; // <-- contraseña de aplicación Gmail
+    public static final String SMTP_HOST        = "smtp.gmail.com";
+    public static final String SMTP_PORT        = "587";
+    public static final String NOMBRE_SISTEMA   = "Autoverse";
+}
