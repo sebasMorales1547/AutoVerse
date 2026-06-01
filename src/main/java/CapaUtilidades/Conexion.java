ba76326302eeaa575462ab97753d1e6d97c7f0b4
Sebastian Morales <sebasandres.1547@gmail.com>
2026-05-31 21:58:03 -0500

    fix: cambiar conexion y credenciales incorrectas

--- src/main/java/CapaUtilidades/Conexion.java
+++ src/main/java/CapaUtilidades/Conexion.java
@@ -1,12 +1,23 @@
 package CapaUtilidades;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexion {
-    private static final String URL = "jdbc:mysql://localhost:3306/autoverse_db";
-    private static final String USER = "root";
-    private static final String PASS = "";
 
-    public static Connection getConexion() throws SQLException {
+    private static final String URL =
+            "jdbc:oracle:thin:@localhost:1521/xepdb1";
+
+    private static final String USER = "autoverse";
+
+    private static final String PASS = "1234";
+
+   public static Connection getConexion() throws SQLException {
+    try {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
         return DriverManager.getConnection(URL, USER, PASS);
+    } catch (ClassNotFoundException e) {
+        throw new SQLException("Driver Oracle no encontrado", e);
+    }
     }
 }
--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -3,9 +3,26 @@ package view;
 import java.awt.Desktop;
 import java.io.File;
 
+import CapaUtilidades.Conexion;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        
+
+        try {
+
+            Conexion.getConexion();
+
+            System.out.println("Conexion exitosa");
+
+        } catch (Exception e) {
+
+            System.out.println("Error de conexion");
+
+            e.printStackTrace();
+        }
 
         try {
 
